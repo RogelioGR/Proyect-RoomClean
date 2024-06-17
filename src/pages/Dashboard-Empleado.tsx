8913629e7a7e453d34e0b0ffa49d745296dd2e7f
Rogelio García Ramos <124382204+RogelioGR@users.noreply.github.com>
2024-06-17 17:02:23 -0500

    subiendo cambios para resolver

--- src/pages/Dashboard-Empleado.tsx
+++ src/pages/Dashboard-Empleado.tsx
@@ -2,9 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
-
-
-/*Componentes */
+/* Componentes */
 import Footer from '../Components/Footer';
 import Header from '../Components/Header';
 import Sidebar from '../Components/Sidebar';
@@ -12,6 +10,7 @@ import Loader from '../Components/Loader';
 
 const Dashboardempleado: React.FC = () => {
     const [loading, setLoading] = useState(true);
+
     useEffect(() => {
         // Simula una carga de datos
         setTimeout(() => {
@@ -19,12 +18,8 @@ const Dashboardempleado: React.FC = () => {
         }, 2000); // el tiempo de carga real
     }, []);
 
-
     const rooms = [
-        { id: 'A01-105', status: 'Por hacer' },
-      
-
-       
+         { id: 'A01-105', status: 'Por hacer' },
     ];
 
     return (
@@ -36,25 +31,31 @@ const Dashboardempleado: React.FC = () => {
                     <Sidebar />
                     <div className="flex-grow-1 d-flex flex-column">
                         <Header />
-                        <div >
+                        <div>
                             <Container fluid className="flex-grow-1 d-flex flex-column">
                                 <h1 className="my-4">Bienvenid@, peki!</h1>
                                 <h3>Habitaciones</h3>
                                 <div className="scroll-container flex-grow-1">
                                     <div className="row">
-                                        {rooms.map((room, index) => (
-                                            <div key={index} className="col-md-3 mb-3">
-                                                <div className="card ">
-                                                    <img src="public/habitacion_Sencilla_8.jpg" className="card-img-top" alt="Room" />
-                                                    <Link to="/TaskEmpleado" style={{ textDecoration: 'none', color: '#000000' }}>
-                                                        <div className="card-body">
-                                                            <h5 className="card-title">{room.id}</h5>
-                                                            <p className="card-text">Estado: <strong>{room.status}</strong></p>
-                                                        </div>
-                                                    </Link>
-                                                </div>
+                                        {rooms.length === 0 ? (
+                                            <div className="col-12 text-center">
+                                                <p>Sin actividades</p>
                                             </div>
-                                        ))}
+                                        ) : (
+                                            rooms.map((room, index) => (
+                                                <div key={index} className="col-md-3 mb-3">
+                                                    <div className="card">
+                                                        <img src="public/habitacion_Sencilla_8.jpg" className="card-img-top" alt="Room" />
+                                                        <Link to="/TaskEmpleado" style={{ textDecoration: 'none', color: '#000000' }}>
+                                                            <div className="card-body">
+                                                                <h5 className="card-title">{room.id}</h5>
+                                                                <p className="card-text">Estado: <strong>{room.status}</strong></p>
+                                                            </div>
+                                                        </Link>
+                                                    </div>
+                                                </div>
+                                            ))
+                                        )}
                                     </div>
                                 </div>
                             </Container>
