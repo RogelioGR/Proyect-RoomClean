d53752dba708ca8c1f30f3926f1199341f140b79
PekiDroiid <124383137+PekiDroiid@users.noreply.github.com>
2024-06-18 14:13:08 -0500

    Corrección de rutas en servicios

--- src/Services/UsuarioService.ts
+++ src/Services/UsuarioService.ts
@@ -1,31 +1,30 @@
 import axiosInstance from './axiosConfig';
 
-// Obtener todos los usuarios
 export const getUsers = async (): Promise<unknown[]> => {
-    const response = await axiosInstance.get('/api/Admin/users');
+    const response = await axiosInstance.get('/Usuario/list');
     return response.data;
 };
 
-// Obtener un usuario por ID
+
 export const getUserById = async (id: string): Promise<unknown> => {
-    const response = await axiosInstance.get(`/users/${id}`);
+    const response = await axiosInstance.get(`/Usuario/list/${id}`);
     return response.data;
 };
 
-// Crear un nuevo usuario
+
 export const createUser = async (userData: unknown): Promise<unknown> => {
-    const response = await axiosInstance.post('/Tarea/list/${id}', userData);
+    const response = await axiosInstance.post('/Usuario/create', userData);
     return response.data;
 };
 
-// Actualizar un usuario existente
+
 export const updateUser = async (id: string, userData: unknown): Promise<unknown> => {
-    const response = await axiosInstance.put(`/users/${id}`, userData);
+    const response = await axiosInstance.put(`/Usuario/update/${id}`, userData);
     return response.data;
 };
 
-// Eliminar un usuario
+
 export const deleteUser = async (id: string): Promise<unknown> => {
-    const response = await axiosInstance.delete(`/users/${id}`);
+    const response = await axiosInstance.delete(`/Usuario/delete/${id}`);
     return response.data;
 };
