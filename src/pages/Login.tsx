d78981a576c95a7b31b1eace666ef904d1e2f242
Rogelio García Ramos <124382204+RogelioGR@users.noreply.github.com>
2024-06-17 19:36:05 -0500

    Cambios en el login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import { Form, Container, Row, Col } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Form, Button, Container, Row, Col } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
-import Swal from 'sweetalert2';
 import axios from 'axios';
 
 const Login: React.FC = () => {
@@ -9,79 +8,53 @@ const Login: React.FC = () => {
     const [password, setPassword] = useState<string>('');
     const navigate = useNavigate();
 
-    // Redirigir a DashboardAdmin si el usuario ya está autenticado
-    useEffect(() => {
-        const token = localStorage.getItem('token');
-        if (token) {
-            navigate('/DashboardAdmin');
-        }
-    }, [navigate]);
-
     const handleLogin = async (event: React.FormEvent) => {
-        event.preventDefault();
-
-        // Validación de la contraseña
-        if (password.length < 8) {
-            Swal.fire({
-                title: 'Contraseña inválida',
-                text: 'La contraseña debe tener al menos 8 caracteres.',
-                icon: 'error',
-                confirmButtonText: 'Aceptar'
-            });
-            return;
-        }
+        event.preventDefault(); // Previene el comportamiento predeterminado del formulario
 
         try {
             const response = await axios.post('https://localhost:7103/api/auth/login', {
                 correo: email,
                 contraseña: password
             });
+            alert('Login successful');
             localStorage.setItem('token', response.data.token);
-            navigate('/DashboardAdmin');
+            navigate("/DashboardAdmin");
+
+            console.log(response.data);
         } catch (error) {
-            Swal.fire({
-                title: 'Usuario inválido',
-                text: 'Correo o contraseña incorrecta.',
-                icon: 'error',
-                confirmButtonText: 'Aceptar'
-            });
+            console.error(error);
+            alert('Login failed');
         }
     };
 
     return (
         <Container fluid className="login-container">
             <Row className="justify-content-center align-items-center vh-100">
-                <Col xs={10} sm={8} md={6} lg={4} className="text-center">
-                    <Form onSubmit={handleLogin} className="form-controls">
-                        <h1 className="title">Iniciar sesión</h1>
-                        <div className="input-field">
-                            <input
-                                required
-                                className="input"
-                                type="email"
-                                placeholder="Correo Electrónico"
-                                value={email}
-                                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
-                                autoComplete="off"
+                <Col md={6} lg={4} className="text-center">
+                    <h1 className="mb-4">Iniciar sesión</h1>
+                    <Form onSubmit={handleLogin}>
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Control 
+                                type="email" 
+                                placeholder="Correo Electrónico" 
+                                value={email} 
+                                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)} 
                             />
-                            <label className="label" htmlFor="input">Correo electrónico</label>
-                        </div>
-                        <div className="input-field">
-                            <input
-                                required
-                                className="input"
-                                type="password"
-                                placeholder="Password"
-                                value={password}
-                                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
-                                autoComplete="off"
+                        </Form.Group>
+                        <Form.Group controlId="formBasicPassword" className="mt-3">
+                            <Form.Control 
+                                type="password" 
+                                placeholder="Password" 
+                                value={password} 
+                                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)} 
                             />
-                            <label className="label" htmlFor="input">Contraseña</label>
-                        </div>
-                        <button className="submit-btn" type="submit">Iniciar sesión</button>
+                        </Form.Group>
+                        <Button variant="primary" type="submit" className="mt-3">
+                            Iniciar sesión
+                        </Button>
                     </Form>
                 </Col>
-                <Col xs={10} sm={8} md={6} lg={4} className="d-none d-lg-block text-center">
+                <Col md={6} lg={4} className="d-none d-md-block text-center">
                     <img src="/public/roomclean.png" alt="Logo" className="login-logo" />
                 </Col>
             </Row>
@@ -89,4 +62,4 @@ const Login: React.FC = () => {
     );
 };
 
-export default Login;
+export default Login;
